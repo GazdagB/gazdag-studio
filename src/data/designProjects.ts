0150fb9a9c0dfa0569614609193e0280ba772152
GazdagB <gazdagbal@gmail.com>
2026-02-15 18:33:09 +0100

    ✏️ Fixed Title typo

--- src/data/designProjects.ts
+++ src/data/designProjects.ts
@@ -6,7 +6,7 @@ export const designProjects : CaseProjects[] = [
   {
     id: 0,
     slug: "erkans-doner",
-    title: "Logo Desing for Erkan's Döner",
+    title: "Logo Design for Erkan's Döner",
     coverImg: "/projects/design/erkans/erkans_doner_horizontal.png",
     img: "/projects/design/erkans/erkans_doner_horizontal.png",
     year: "2025",
